aab28fde778adc52a61481ee72698fc0cdeb26cb
Luis <looes1@gmail.com>
2022-06-29 17:06:24 +0200

    add blog post page skeleton

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Link, useStaticQuery, graphql } from "gatsby";
 import { 
     container,
+    siteTitle,
     heading,
     navLinks,
     navLinkItem,
@@ -22,11 +23,12 @@ const Layout = ({ pageTitle, children}) => {
     return (
         <div className={container}>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <header>{data.site.siteMetadata.title}</header>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to='/' className={navLinkText}>Home</Link></li>
                     <li className={navLinkItem}><Link to='/about' className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to='/blog' className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+    return (
+       <Layout pageTitle="Blog Posts">
+        <p>Posts will go here</p>
+       </Layout> 
+    )
+}
+
+export default BlogPage
