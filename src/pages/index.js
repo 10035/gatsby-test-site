4ef955ac1b95caee22250325198086484d8f9329
Luis <looes1@gmail.com>
2022-06-29 14:53:33 +0200

    add image from local file system

--- src/pages/index.js
+++ src/pages/index.js
@@ -137,6 +137,10 @@ const IndexPage = () => {
         alt="Swiss Mountain"
         src="https://images.unsplash.com/photo-1580935422787-b83428ed8271?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1618&q=80"
       />
+      <StaticImage
+        alt="Melbourne, Australia"
+        src="../images/melbourne.jpg"
+      />
     </Layout>
   )
 }
