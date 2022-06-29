a055ef770acbc935d29f2eb43aa274d63ea7bae4
Luis <looes1@gmail.com>
2022-06-29 17:20:38 +0200

    fix typos config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-  {
-    resolve: "gatsby-source-file-system",
-    options: {
-      name: 'blog',
-      path: '${__dirname}/blog',
-    }
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
