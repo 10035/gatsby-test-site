e19e8dd6e8ac228a6e715e6fd9e758612ab35bec
Luis <looes1@gmail.com>
2022-06-29 17:17:03 +0200

    add blog directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,11 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-source-file-system",
+    options: {
+      name: 'blog',
+      path: '${__dirname}/blog',
+    }
   ],
 }
