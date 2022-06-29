ee1cd16a0b1be6d73fab2100d8a0f35b12735902
Luis <looes1@gmail.com>
2022-06-29 13:43:53 +0200

    update homepage content, add new about page component, add link component to pages, create and style reusable layout component

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import Layout from "../components/layout";
+
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there, I'm Luis!</p>
+        </Layout>
+    )
+}
+
+
+// const AboutPage = () => {
+//     return (
+//         <main>
+//             <title>About Me</title>
+//             <h1>About Me</h1>
+//             <Link to='/'>Home Page</Link>
+//             <p>Hi there, I'm Luis!</p>
+//         </main>
+//     )
+// }
+
+
+export default AboutPage
