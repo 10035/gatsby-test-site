8e1f809897110f5f6991d0a163f8e9d1cd38c0b8
Luis <looes1@gmail.com>
2022-07-02 02:56:06 +0200

    implement dynamic images render functionality

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,19 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image/'
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
+
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.date}</p>
+            <p>Posted: {data.mdx.frontmatter.datePublished}</p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -14,16 +21,35 @@ const BlogPost = ({ data }) => {
     )
 }
 
+
 export const query = graphql`
-query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        datePublished(formatString: "MMMM D, YYYY")
-      }
-      body
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            body
+            frontmatter {
+                title
+                datePublished(formatString: "MMMM D, YYYY")
+                hero_image_alt
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+            }
+        }
     }
-  }
 `
+// export const query = graphql`
+// query ($id: String) {
+//     mdx(id: {eq: $id}) {
+//       frontmatter {
+//         title
+//         datePublished(formatString: "MMMM D, YYYY")
+//       }
+//       body
+//     }
+//   }
+// `
+  
 
 export default BlogPost
